aa33efa46b6da47072a2025a04abdecbcf17be32
Argledargle <apyells@gmail.com>
2020-09-06 18:22:51 -0500

    adds searcfh functionaltiy

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,23 +1,39 @@
-import React from "react";
-import Search from "../Search";
+import React from 'react'
+import Search from '../Search'
+import Cards from '../Cards'
 
-function Main() {
-  return (
-    <div>
-      <div className="max-w-lg mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md">
-        <div className="ml-6 pt-1">
-          <h1 className="text-2xl text-blue-700 leading-tight">
-            Clear the Fridge
-          </h1>
-          <p className="text-base text-gray-700 leading-normal">
-            A project to help you get the most of whatever you have in your
-            fridge or pantry
-          </p>
+class Main extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { searchResults: null }
+  }
+
+  searchResults = value => {
+    console.log('Parent received vlaue from child: ', value)
+    this.setState({ searchResults: value })
+  }
+
+  render () {
+    console.log(this.state)
+    return (
+      <div>
+        <div className='max-w-lg mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md'>
+          <div className='ml-6 pt-1'>
+            <h1 className='text-2xl text-blue-700 leading-tight'>
+              Clear the Fridge
+            </h1>
+            <p className='text-base text-gray-700 leading-normal'>
+              A project to help you get the most of whatever you have in your
+              fridge or pantry
+            </p>
+          </div>
         </div>
+        <Search callback={this.searchResults} />
+        {console.log(this.state)}
+        <Cards />
       </div>
-      <Search />
-    </div>
-  );
+    )
+  }
 }
 
-export default Main;
+export default Main
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,64 +1,175 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from 'react'
+import _pull from 'lodash/pull'
 
-const Form = () => {
-  const [input, setInput] = useState(undefined);
-  const handleInputChange = (e) =>
+const url = 'https://api.edamam.com/search?'
+const appId = 'c8b6b757'
+const appKey = '112f88722937558772de9b30b52b63ed'
+
+const dietArray = [
+  {
+    name: 'Vegan',
+    value: 'vegan'
+  },
+  {
+    name: 'Vegetarian',
+    value: 'vegetarian'
+  },
+  {
+    name: 'Sugar Conscious',
+    value: 'sugar-conscious'
+  },
+  {
+    name: 'Peanut Free',
+    value: 'peanut-free'
+  },
+  {
+    name: 'Tree-nut Free',
+    value: 'tree-nut-free'
+  },
+  {
+    name: 'Alcohol Free',
+    value: 'alcohol-free'
+  }
+]
+
+const Search = ({ callback }) => {
+  const [input, setInput] = useState(undefined)
+
+  const handleInputChange = e =>
     setInput({
       ...input,
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  console.log(input);
-  return (
-    <form className="mx-auto flex flex-row justify-between">
-      <input
-        className="rounded-lg border-blue-900 border"
-        name="textInput"
-        type="text"
-        placeholder="Chicken, rice"
-        id="search-term"
-        required
-        onChange={handleInputChange}
-      />
-      <select
-        className="rounded-lg border-blue-900 border"
-        name="dietInput"
-        onChange={handleInputChange}
-      >
-        <option value="" defaultValue>
-          Select
-        </option>
-        <option value="balanced">Balanced</option>
-        <option value="high-fiber">High-Fiber</option>
-        <option value="high-protein">High-Protein</option>
-        <option value="low-carb">Low-Carb</option>
-        <option value="low-fat">Low-Fat</option>
-        <option value="low-sodium">Low-Sodium</option>
-      </select>
-      <input
-        type="submit"
-        value="Search"
-        className="rounded-lg border-blue-900 border"
-      />
-    </form>
-  );
-};
+      [e.currentTarget.name]: e.currentTarget.value
+    })
 
-class Search extends React.Component {
-  render() {
-    return (
-      <div className="max-w-lg flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md mx-auto">
-        <div className="ml-6 pt-1">
-          <h1 className="text-2xl text-blue-700 leading-tight">
-            What do you have?
-          </h1>
-          <p className="text-base text-gray-700 leading-normal">
-            Type in the ingredients you're looking to clear out from your
-            kitchen, select a diet and click search.
-          </p>
-          <Form />
-        </div>
-      </div>
-    );
+  const [checkedDietArray, dispatch] = useReducer(
+    (checkedDietArray, { type, value }) => {
+      switch (type) {
+        case 'add':
+          return [...checkedDietArray, value]
+        case 'remove':
+          return _pull(checkedDietArray, value)
+        default:
+          return checkedDietArray
+      }
+    },
+    []
+  )
+
+  function buildSearchQuery (e) {
+    e.preventDefault()
+    if (
+      input === undefined ||
+      input.textInput === undefined ||
+      input.textInput === ''
+    ) {
+      alert('You need to enter something into the text field.')
+      return null
+    }
+    const searchUrl = formatQueryParams(input)
+    getResults(searchUrl)
+
+    return formatQueryParams(input)
   }
+
+  function getResults (url) {
+    callback('loading')
+    fetch(url)
+      .then(response => response.json())
+      .then(data => callback(data))
+  }
+
+  function formatQueryParams (params) {
+    const healthQuery = checkedDietArray.map(item => {
+      return `health=${item}&`
+    }).toString()
+
+    const queryStringComponents = {
+      app_key: appKey,
+      app_id: appId
+    }
+
+    if (params.textInput !== '' || params.textInput !== undefined) {
+      queryStringComponents.q = params.textInput
+    }
+
+    if (params.dietInput !== '' || params.dietInput !== undefined) {
+      queryStringComponents.diet = params.dietInput
+    }
+
+    const queryString = Object.keys(queryStringComponents)
+      .map(key => `${key}=${queryStringComponents[key]}`)
+      .join('&')
+    const newUrl = url + healthQuery  + queryString
+    return newUrl
+  }
+
+  return (
+    <div className='max-w-lg flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md mx-auto'>
+      <div className='ml-6 pt-1'>
+        <h1 className='text-xl text-blue-700 leading-tight'>
+          What do you want to use up?
+        </h1>
+        <p className='text-base text-gray-700 leading-normal'>
+          Type in the ingredients you're looking to clear out from your kitchen,
+          select a diet and click search.
+        </p>
+        <form className='mx-auto flex flex-row justify-between flex-wrap'>
+          <input
+            className='rounded-sm border border-gray-600'
+            name='textInput'
+            type='text'
+            placeholder='Chicken, rice'
+            id='search-term'
+            required
+            onChange={handleInputChange}
+          />
+          <select
+            className='rounded-sm border-gray-600 border'
+            name='dietInput'
+            onChange={handleInputChange}
+          >
+            <option value='' defaultValue>
+              Select
+            </option>
+            <option value='balanced'>Balanced</option>
+            <option value='high-protein'>High-Protein</option>
+            <option value='low-carb'>Low-Carb</option>
+            <option value='low-fat'>Low-Fat</option>
+          </select>
+          <input
+            type='submit'
+            value='Search'
+            className='rounded-sm border-gray-600 border'
+            onClick={buildSearchQuery}
+          />
+          <fieldset>
+            <legend>Optional health choices:</legend>
+            {dietArray.map(item => {
+              return (
+                <React.Fragment key={item.value}>
+                  <input
+                    type='checkbox'
+                    name={item.value}
+                    value={item.value}
+                    id={item.value}
+                    onChange={() =>
+                      dispatch({
+                        type: checkedDietArray.includes(item.value)
+                          ? 'remove'
+                          : 'add',
+                        value: item.value
+                      })
+                    }
+                  />
+                  <label htmlFor={item.value}>{item.name}</label>
+                </React.Fragment>
+              )
+            })}
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  )
 }
-export default Search;
+
+export default Search
