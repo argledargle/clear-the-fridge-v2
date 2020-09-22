36e6d27f3f76ecfd39f48c349afbc25efae4dd09
Alex <alexpeter@northwesternmutual.com>
2020-09-21 22:36:30 -0500

    Updates

---
+++ precache-manifest.e5053d8e39b6d5bc5e33a5b67301f3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4444fa8daa05d52fdeb19a34562fc50f",
+    "url": "/clear-the-fridge-v2/index.html"
+  },
+  {
+    "revision": "599409451a03ea721220",
+    "url": "/clear-the-fridge-v2/static/css/main.232c872a.chunk.css"
+  },
+  {
+    "revision": "85bab6c289e94bbeff0b",
+    "url": "/clear-the-fridge-v2/static/js/2.7193d26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clear-the-fridge-v2/static/js/2.7193d26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599409451a03ea721220",
+    "url": "/clear-the-fridge-v2/static/js/main.4d856ea0.chunk.js"
+  },
+  {
+    "revision": "123b77b34387503ba7f6",
+    "url": "/clear-the-fridge-v2/static/js/runtime-main.7be2920a.js"
+  }
+]);
