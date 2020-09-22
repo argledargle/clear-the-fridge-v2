21dfcd8d2d799ef965725e7fe7775e9e4eb59915
Alex <alexpeter@northwesternmutual.com>
2020-09-21 22:09:41 -0500

    fix: functional component refactor

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,34 +1,27 @@
 import React from 'react'
 import Card from '../Card'
 
-class Main extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { ...props }
-  }
-  render () {
-    const recipeArray = this.state.recipes
-
-    if (this.props.length === 0) {
-      return (
-        <div className='max-w-lg mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md'>
-          <h1 className='text-2xl text-blue-700 leading-tight'>
-            Sorry. No results were found. :(
-          </h1>
-          <h2 className='text-lg text-blue-700 leading-tight'>
-            Try changing your search terms or diet.
-          </h2>
-        </div>
-      )
-    }
+function Main (props) {
+  const recipeArray = props.recipes
+  if (props.length === 0) {
     return (
-      <div className='max-w-lg mx-auto flex flex-col mt-10'>
-        {recipeArray.map((item, i) => {
-          const recipe = item.recipe
-          return <Card key={i} {...recipe} />
-        })}
+      <div className='max-w-lg mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-md'>
+        <h1 className='text-2xl text-blue-700 leading-tight'>
+          Sorry. No results were found. :(
+        </h1>
+        <h2 className='text-lg text-blue-700 leading-tight'>
+          Try changing your search terms or diet.
+        </h2>
       </div>
     )
   }
+  return (
+    <div className='max-w-lg mx-auto flex flex-col mt-10'>
+      {recipeArray.map((item, i) => {
+        const recipe = item.recipe
+        return <Card key={i} {...recipe} />
+      })}
+    </div>
+  )
 }
 export default Main
