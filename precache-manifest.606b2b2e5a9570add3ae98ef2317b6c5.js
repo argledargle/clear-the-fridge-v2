3b3014672d8be401c156c687a5dcec278a8053e3
Alex <alexpeter@northwesternmutual.com>
2020-09-24 20:25:30 -0500

    fix: merge issue

---
+++ precache-manifest.606b2b2e5a9570add3ae98ef2317b6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb404ce5fa98ac73de7453070dcef7",
+    "url": "/clear-the-fridge-v2/index.html"
+  },
+  {
+    "revision": "49fb48e398e410580b26",
+    "url": "/clear-the-fridge-v2/static/css/main.fd2e6272.chunk.css"
+  },
+  {
+    "revision": "85bab6c289e94bbeff0b",
+    "url": "/clear-the-fridge-v2/static/js/2.7193d26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clear-the-fridge-v2/static/js/2.7193d26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fb48e398e410580b26",
+    "url": "/clear-the-fridge-v2/static/js/main.0611d0f4.chunk.js"
+  },
+  {
+    "revision": "123b77b34387503ba7f6",
+    "url": "/clear-the-fridge-v2/static/js/runtime-main.7be2920a.js"
+  }
+]);
