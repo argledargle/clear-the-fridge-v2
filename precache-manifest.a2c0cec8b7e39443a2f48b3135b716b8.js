70366ae4b8e368ae2870011db686397c5fcf286a
Alex Peter <apyells@gmail.com>
2020-10-24 14:38:35 -0500

    Updates

---
+++ precache-manifest.a2c0cec8b7e39443a2f48b3135b716b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0fae5793d93b32efca6ee0980a6b5f",
+    "url": "/clear-the-fridge-v2/index.html"
+  },
+  {
+    "revision": "124572854086ad3165be",
+    "url": "/clear-the-fridge-v2/static/css/main.cb58b6c5.chunk.css"
+  },
+  {
+    "revision": "d80eccc511e77b4541fd",
+    "url": "/clear-the-fridge-v2/static/js/2.daaee1b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clear-the-fridge-v2/static/js/2.daaee1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124572854086ad3165be",
+    "url": "/clear-the-fridge-v2/static/js/main.cd280ca5.chunk.js"
+  },
+  {
+    "revision": "123b77b34387503ba7f6",
+    "url": "/clear-the-fridge-v2/static/js/runtime-main.7be2920a.js"
+  },
+  {
+    "revision": "a2251bf98616ab7a0e744eb8fb7b87ae",
+    "url": "/clear-the-fridge-v2/static/media/external-link.a2251bf9.svg"
+  }
+]);
